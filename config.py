987e61c76b52db8864d4e4a497053eaed829211f
Vincenthe3231 <112159069+Vincenthe3231@users.noreply.github.com>
2025-08-20 17:39:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cddfuznj1a65.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lawwensen-employee"
+customregion = "us-east-1"
+
